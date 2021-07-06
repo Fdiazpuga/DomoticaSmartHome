15642f30d6a708ae53d4e2232255d3691e00509d
Fabian <fadiazpuga@gmail.com>
2021-07-06 16:49:14 -0400

    Conexión WIFI a Red de Prueba

--- DomoticaSmartHome/src/main.cpp
+++ DomoticaSmartHome/src/main.cpp
@@ -1,10 +1,25 @@
 #include <Arduino.h>
-
+#include "WiFi.h"
+ 
+const char* ssid = "DevOps";
+const char* password =  "IOce7SmKV|b@Wmd\W]DX";
+ 
 void setup() {
-  // put your setup code here, to run once:
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+ 
 }
-
+ 
+void loop() {}
 void loop() {
   // put your main code here, to run repeatedly:
 }
-asdsadsaadsds
