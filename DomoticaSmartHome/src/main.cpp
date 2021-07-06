3bb5c31397fad37a3127db02361c159c5796bb64
Fabian <fadiazpuga@gmail.com>
2021-07-06 17:29:29 -0400

    Se añadió led indicador de conexión wifi

--- DomoticaSmartHome/src/main.cpp
+++ DomoticaSmartHome/src/main.cpp
@@ -1,22 +1,28 @@
 #include <Arduino.h>
 #include "WiFi.h"
+
  
 const char* ssid = "DevOps";
 const char* password =  "CMJGMww8I1oVUQwqyB8B";
- 
+
+#define LED 2
+
 void setup() {
  
   Serial.begin(115200);
- 
+  pinMode(LED, OUTPUT);
+
   WiFi.begin(ssid, password);
  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
+    
   }
  
   Serial.println("Connected to the WiFi network");
- 
+  digitalWrite(LED, HIGH); //Led de placa indica que está conectado a wifi
+  delay(250);
 }
  
  
